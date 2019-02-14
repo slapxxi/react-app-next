8244f7e9a4adcf88f22fc186db62f2a0d47244b1
slavapavlutin <sl.pavlutin@gmail.com>
2019-02-14 23:33:38 +0300

    Add bundle analysis scripts

--- next.config.js
+++ next.config.js
@@ -4,60 +4,73 @@ let dotenv = require('dotenv');
 let withTypescript = require('@zeit/next-typescript');
 let withTM = require('next-transpile-modules');
 let withCSS = require('@zeit/next-css');
+let withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
 dotenv.config();
 
-module.exports = withTypescript(
-  withCSS(
-    withTM({
-      target: 'serverless',
+let nextConfig = {
+  target: 'serverless',
 
-      cssModules: true,
+  cssModules: true,
 
-      cssLoaderOptions: {
-        modules: true,
-        camelCase: true,
-        namedExport: true,
-        importLoaders: 1,
-        localIdentName: '[local]__[hash:base64:5]',
-      },
+  cssLoaderOptions: {
+    modules: true,
+    camelCase: true,
+    namedExport: true,
+    importLoaders: 1,
+    localIdentName: '[local]__[hash:base64:5]',
+  },
 
-      transpileModules: ['lodash-es'],
+  transpileModules: ['lodash-es'],
 
-      env: {
-        firebase: {
-          apiKey: process.env.FIREBASE_API_KEY,
-          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-          databaseURL: process.env.FIREBASE_DATABASE_URL,
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        },
-      },
+  env: {
+    firebase: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.FIREBASE_DATABASE_URL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    },
+  },
 
-      webpack(config, options) {
-        if (!options.isServer) {
-          for (let entry of options.defaultLoaders.css) {
-            if (entry.loader === 'css-loader') {
-              entry.loader = 'typings-for-css-modules-loader';
-              break;
-            }
-          }
+  analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/server.html',
+    },
+    browser: {
+      analyzerMode: 'static',
+      reportFilename: '../bundles/client.html',
+    },
+  },
+
+  webpack(config, options) {
+    if (!options.isServer) {
+      for (let entry of options.defaultLoaders.css) {
+        if (entry.loader === 'css-loader') {
+          entry.loader = 'typings-for-css-modules-loader';
+          break;
         }
+      }
+    }
 
-        config.resolve.alias = {
-          ...config.resolve.alias,
-          '@self': path.resolve(__dirname),
-        };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@self': path.resolve(__dirname),
+    };
 
-        config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
 
-        return config;
-      },
+    return config;
+  },
 
-      exportPathMap() {
-        return {};
-      },
-    }),
-  ),
-);
+  exportPathMap() {
+    return {};
+  },
+};
+
+module.exports = withBundleAnalyzer(withTypescript(withCSS(withTM(nextConfig))));
